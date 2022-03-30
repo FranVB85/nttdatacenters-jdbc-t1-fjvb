40278db7316af6273e4cf9dc6b259f35b7924e59
fvasqueb <fvasqueb@emeal.nttdata.com>
2022-03-30 09:21:12 +0200

    first commit

---
+++ src/main/java/com/everis/nttdatacenters_jdbc_t1_fjvb/App.java
@@ -0,0 +1,71 @@
+package com.everis.nttdatacenters_jdbc_t1_fjvb;
+
+import java.sql.DriverManager;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main( String[] args )
+	{
+
+		// Conectar
+		Conecction();
+
+	}
+
+	private static void Conecction() {
+
+		// Datos de la conexión
+		String usuario = "usuario";
+		String clave = "clave";
+		Connection dbConnection = null;
+		Statement statement = null;
+
+		try {
+
+			// Driver de la conexión
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// Conectar
+			dbConnection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", usuario, clave);
+
+			// Consulta
+			statement = dbConnection.createStatement();
+			final String query = "SELECT id, username, password, nombre FROM Usuarios";;
+			final ResultSet rs = statement.executeQuery(query);
+
+			// Tratar los datos
+			StringBuilder user1 = new StringBuilder();
+			while(rs.next()) {
+				String id = rs.getString("ID");
+				String usr = rs.getString("USERNAME");
+				String psw = rs.getString("PASSWORD");
+				String nombre = rs.getString("NOMBRE");
+				System.out.println("userid : " + id);
+				System.out.println("usr : " + usr);
+				System.out.println("psw : " + psw);
+				System.out.println("nombre : " + nombre);
+			}
+
+
+			// Cerrar conexión
+			dbConnection.close();
+
+			// En caso de excepción
+		} catch (ClassNotFoundException | SQLException e) {
+
+			// Mostrar por consola
+			System.out.println( "ERROR CONEXION" );
+
+		}
+	}
+}
